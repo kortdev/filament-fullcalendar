ce40ecbfa2968bca429c2677359bfe88793274ca
LTKort <lars.kort94@gmail.com>
2023-01-27 13:40:18 +0100

    style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-    mode: 'jit',
+    mode: "jit",
 
-    content: [
-        './resources/views/**/*.blade.php',
-    ],
+    content: ["./resources/views/**/*.blade.php"],
 
     theme: {
         extend: {
             colors: {
-                primary: colors.orange,
+                primary: {
+                    50: "#fef8ed",
+                    100: "#f8edd2",
+                    150: "#f5e3c2",
+                    200: "#f0dba6",
+                    300: "#e9c979",
+                    400: "#e1b74d",
+                    500: "#daa520",
+                    DEFAULT: "#daa520",
+                    600: "#ae841a",
+                    700: "#836313",
+                    800: "#57420d",
+                    900: "#2c2106",
+                },
             },
         },
     },
